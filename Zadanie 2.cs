1f8af245e2ffa5b1efe45e76f05320ec40775c42
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-11-30 09:59:00 +0300

    Add files via upload

---
+++ Zadanie 2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zadanie_1._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Двузначное число: ");
+            int num = int.Parse(Console.ReadLine());
+            int sum = num % 10 + (num / 10) % 10;
+            Console.WriteLine((sum % 3) == 0 ? "Кратно" : "Не кратно");
+            Console.ReadKey(true);
+        }
+    }
+}
