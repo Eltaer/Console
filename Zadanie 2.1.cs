c819c7bec1354c7a2bb655832be5798930d1cb05
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-01 09:08:57 +0300

    Add files via upload

---
+++ Zadanie 2.1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadanie_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Введите x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            y = double.Parse(Console.ReadLine());
+            if ((x*x + y*y < 144) && y-Math.Abs(x) > 0)
+            {
+                Console.WriteLine("Точка попала в заштрихованную область");
+            }
+            else if ((x * x + y * y == 144) && y - Math.Abs(x) == 0)
+
+            {
+                Console.WriteLine("Точка на границе заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не попала в заштрихованную область");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Zadanie 2.2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zadanie_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество пройденных месяцев: ");
+            int m = Int32.Parse(Console.ReadLine());
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+            Console.WriteLine(months[DateTime.Now.AddMonths(-m).Month - 1]);
+        }
+    }
+}
