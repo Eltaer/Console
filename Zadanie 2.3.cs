6b2d6c712732cf21e01f0fea2966d032cd1e197f
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-01 14:36:03 +0300

    Add files via upload

---
+++ Zadanie 2.3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Цикл For: ");
+
+            for (int i = B; i >= A; i--) 
+            { 
+                Console.WriteLine(i * i * i + " "); 
+            }
+
+            Console.WriteLine("Цикл While: ");
+
+            int c = B;
+            while (c >= A)
+            {
+                Console.WriteLine( c * c * c + " ");
+                c--;
+            }
+
+            Console.WriteLine("Цикл Do While: ");
+
+            int x = B;
+            do
+            {
+                Console.WriteLine(x * x * x + " ");
+                x--;
+            }
+            while (x >= A);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie 2.4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadanie_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i; j++)
+                    Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
