e40634f968fe73f3286dfc87753fd8d7bfd579ff
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-14 09:36:30 +0300

    Update Zadanie 3.1.cs

--- Zadanie 3.1.cs
+++ Zadanie 3.1.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 
-namespace Zadanie_3._1
+namespace _3._1
 {
     class Program
     {
-            static double f(double x)
-            {
+        static double f(double x)
+        {
             return x % 10;
-            }
+        }
 
-            static void Main(string[] args)
-            {
-                Console.Write("Введите a=");
-                double a = double.Parse(Console.ReadLine());
-                Console.Write("Введите b=");
-                double b = double.Parse(Console.ReadLine());
-                double z = f(a) + f(b);
-                Console.WriteLine("z=" + z);
-            }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x= ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите a= ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b= ");
+            double b = double.Parse(Console.ReadLine());
+            double z = f(a) + f(b);
+            Console.WriteLine("z= " + z);
+            Console.ReadKey();
+        }
     }
 }
