bc064bbc2c5f51b3130afaf0c756b401def31a3b
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-15 11:42:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication41
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите строку:");
+                string text = Console.ReadLine();
+                string[] splitted = text.Split(new[] { ' ', '.', '?', '!', ')', '(', ',', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("Введите символ:");
+                char symbol = char.Parse(Console.ReadLine());
+                foreach (string s in splitted)
+                {
+                    if (s.Contains(symbol))
+                    {
+                        text = text.Replace(s, string.Empty);
+                    }
+                }
+                Console.WriteLine(text);
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
