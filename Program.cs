941af7a6e5b7b598b7cb5259b979f2b0229088b0
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-15 11:41:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+            {
+                Console.Write("Введите строку: ");
+                string s = Console.ReadLine();
+                Console.WriteLine(IsThere(s));
+                Console.ReadKey();
+            }
+
+        static bool IsThere(string s)
+            {
+            for (int i = 0; i < s.Length - 1; i++)
+                if (s[i] == s[i + 1])
+                    return true;
+            return false;
+            } 
+    }
+}
