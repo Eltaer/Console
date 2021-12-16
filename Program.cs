0ba324e2e57beaaa269458630576218964a37b13
Eltaer <95209215+Eltaer@users.noreply.github.com>
2021-12-16 09:43:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\stu-pkspk219\Desktop\file2.txt";
+            string longestString = GetLongestString(fileName);
+            Console.WriteLine(longestString);
+            Console.WriteLine("Длина строки: " + longestString.Length);
+            Console.ReadKey();
+        }
+
+        public static string GetLongestString(string fileName)
+        {
+            string currentString, longestString = "";
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    currentString = reader.ReadLine();
+                    if (currentString.Length > longestString.Length)
+                        longestString = currentString;
+                }
+                return longestString;
+            }
+        }
+    }
+}
